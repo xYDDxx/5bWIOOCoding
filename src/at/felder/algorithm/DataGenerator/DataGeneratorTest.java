4601874dffc7f8bead213b9cafa5108c5a34b310
Ian <ianlee.felder@gmail.com>
2024-12-04 13:59:25 +0100

    AlgoDat DataGeneratorTest

--- src/at/felder/algorithm/DataGenerator/DataGeneratorTest.java
+++ src/at/felder/algorithm/DataGenerator/DataGeneratorTest.java
@@ -1,11 +1,20 @@
 package at.felder.algorithm.DataGenerator;
 
+import org.junit.jupiter.api.Assertions;
+
+import javax.xml.crypto.Data;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DataGeneratorTest {
 
     @org.junit.jupiter.api.Test
     void generateDataArray() {
+        int[] data = DataGenerator.generateDataArray(20);
+        for (int element: data) {
+            System.out.println(element);
+        }
+        Assertions.assertEquals(20, data.length);
     }
 
     @org.junit.jupiter.api.Test
