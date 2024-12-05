47a0769d0cb6a4ad3518f0c4b1e8db5722284aea
Ian <ianlee.felder@gmail.com>
2024-12-05 10:50:55 +0100

    AlgoDat BaseAlgorithm no Test

---
+++ src/at/felder/algorithm/BaseAlgo/Base.java
@@ -0,0 +1,33 @@
+package at.felder.algorithm.BaseAlgo;
+
+public class Base {
+
+        public static int[] revert(int[] data) {
+            int[] reversed = new int[data.length];
+            for (int i = 0; i < data.length; i++) {
+                reversed[i] = data[data.length - 1 - i];
+            }
+            return reversed;
+        }
+
+        public static int max(int[] data) {
+            int max = data[0];
+            for (int i = 1; i < data.length; i++) {
+                if (data[i] > max) {
+                    max = data[i];
+                }
+            }
+            return max;
+        }
+
+        public static int min(int[] data) {
+            int min = data[0];
+            for (int i = 1; i < data.length; i++) {
+                if (data[i] < min) {
+                    min = data[i];
+                }
+            }
+            return min;
+        }
+    }
+
